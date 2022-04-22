499c6a27e3b82641386ddfedc13167e13273677a
gjro <gilbertojr.ro@gmail.com>
2022-04-22 16:32:14 -0300

    models criados

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ChecklistRouter = require("./src/routes/checklist");
+require("./config/database");
 
 const app = express();
 app.use(express.json());
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const Checklist = require("../models/checklist");
+
 router.get("/", (req, res) => {
 	console.log("Checklist");
 	res.send();
@@ -10,6 +12,21 @@ router.get("/", (req, res) => {
 router.post("/", (req, res) => {
 	console.log(req.body);
 	res.status(200).json(req.body);
+});
+
+router.get("/:id", (req, res) => {
+	console.log(req.params.id);
+	res.send(`ID: ${req.params.title}`);
+});
+
+router.put("/:id", (req, res) => {
+	console.log(req.params.id);
+	res.send(`ID: ${req.params.id}`);
+});
+
+router.delete("/:id", (req, res) => {
+	console.log(req.params.id);
+	res.send(`ID: ${req.params.id}`);
 });
 
 module.exports = router;
